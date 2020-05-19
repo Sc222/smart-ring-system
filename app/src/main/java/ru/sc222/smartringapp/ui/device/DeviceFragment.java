fb3f83abc714381611a91960d8e8e78ed58be98c
Sc222 <v.a.shaternikov@gmail.com>
2020-05-20 04:23:15 +0500

    device fragment

--- app/src/main/java/ru/sc222/smartringapp/ui/device/DeviceFragment.java
+++ app/src/main/java/ru/sc222/smartringapp/ui/device/DeviceFragment.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -23,13 +25,15 @@ public class DeviceFragment extends Fragment {
         deviceViewModel =
                 ViewModelProviders.of(this).get(DeviceViewModel.class);
         View root = inflater.inflate(R.layout.fragment_device, container, false);
-        final TextView textView = root.findViewById(R.id.text_device);
-        deviceViewModel.getText().observe(this, new Observer<String>() {
+       // final CardView mainCard = root.findViewById(R.id.main_card);
+       //mainCard.setEnabled(false); //changes main device color tint
+       // final TextView textView = root.findViewById(R.id.text_device);
+      /*  deviceViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
         return root;
     }
 }
