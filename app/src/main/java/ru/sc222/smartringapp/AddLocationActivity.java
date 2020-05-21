e3f706731a2bbe2444738c9661d21dcdf68c33af
Sc222 <v.a.shaternikov@gmail.com>
2020-05-21 18:27:58 +0500

    add location activity development

--- app/src/main/java/ru/sc222/smartringapp/AddLocationActivity.java
+++ app/src/main/java/ru/sc222/smartringapp/AddLocationActivity.java
@@ -1,18 +1,20 @@
 package ru.sc222.smartringapp;
 
-import android.graphics.BlendMode;
-import android.graphics.BlendModeColorFilter;
-import android.graphics.ColorFilter;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatSpinner;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddLocationActivity extends AppCompatActivity {
 
@@ -21,25 +23,79 @@ public class AddLocationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_location);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final Toolbar toolbar = findViewById(R.id.toolbar);
 
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
 
         assert actionBar != null;
         actionBar.setDisplayHomeAsUpEnabled(true);
-        //toolbar.getNavigationIcon().setColorFilter(new BlendModeColorFilter(R.color.colorPrimary, BlendMode.SRC_ATOP));
 
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        AppCompatSpinner spinnerBackground = findViewById(R.id.spinner_background);
+        List<String> backgrounds = new ArrayList<>();
+        backgrounds.add("Основной цвет");
+        backgrounds.add("Лес");
+        backgrounds.add("Поле");
+        setupSpinner(spinnerBackground, backgrounds);
+
+        AppCompatSpinner spinnerSingleClick = findViewById(R.id.spinner_single_click);
+        AppCompatSpinner spinnerDoubleClick = findViewById(R.id.spinner_double_click);
+        AppCompatSpinner spinnerTripleClick = findViewById(R.id.spinner_triple_click);
+        AppCompatSpinner spinnerLongPress =  findViewById(R.id.spinner_long_press);
+        List<String> actions = new ArrayList<>();
+        actions.add("Не назначено");
+        actions.add("Включить свет");
+        actions.add("Поставить чайник");
+        actions.add("Включить телевизор");
+        actions.add("Включить компьютер");
+        setupSpinner(spinnerSingleClick, actions);
+        setupSpinner(spinnerDoubleClick, actions);
+        setupSpinner(spinnerTripleClick, actions);
+        setupSpinner(spinnerLongPress, actions);
+
+
+        final TextInputLayout textFieldName=findViewById(R.id.text_field_name);
+        final TextInputLayout textFieldLocation=findViewById(R.id.text_field_location);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String name = textFieldName.getEditText().getText().toString();
+                String location = textFieldLocation.getEditText().getText().toString();
+
+                if(name.equals("")) {
+                    textFieldName.setError("Укажите название локации");
+                }
+                else {
+                    textFieldName.setErrorEnabled(false);
+                }
+
+                if(location.equals("")) {
+                    textFieldLocation.setError("Укажите местоположение локации");
+                }
+                else {
+                    textFieldLocation.setErrorEnabled(false);
+                }
+
+
+                //processInput();
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    private void setupSpinner(AppCompatSpinner spinner, List<String> entries) {
+        // TODO CREATE CUSTOM SPINNER ITEM AND CUSTOM ARRAY ADAPTER
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, entries);
+        dataAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
     }
 
 }
