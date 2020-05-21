57f7e47b4d634a4aff43593729e06ca76c2e7b4d
Sc222 <v.a.shaternikov@gmail.com>
2020-05-21 23:22:40 +0500

    update dependencies

--- app/src/main/java/ru/sc222/smartringapp/MainActivity.java
+++ app/src/main/java/ru/sc222/smartringapp/MainActivity.java
@@ -37,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+
         // navView.setSelectedItemId(R.id.navigation_device);
        // navController.navigate(PreferenceUtils.getCurrentNavigationItem(this));
 
